a0e6e5655532c8d27e6c87272cf8f1167c49877d
jihoahn9303 <rediate03@gmail.com>
2024-02-28 12:28:43 +0900

    Add CI/CD & Update code structure for runtime stability😃

--- database/database_setting.py
+++ database/database_setting.py
@@ -22,7 +22,9 @@ def get_database_url() -> str:
 
 
 async def get_db():
-    engine = create_async_engine(get_database_url())
+    database_url = get_database_url()
+    print('database_url: ', database_url)
+    engine = create_async_engine(url=database_url)
     async_session = async_sessionmaker(bind=engine)
     
     async with async_session() as async_db:
