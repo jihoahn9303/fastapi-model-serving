8bd256ecc86e4cb9360784fb47eb920739e16e0b
jihoahn9303 <rediate03@gmail.com>
2024-02-28 12:55:36 +0900

    Add CI/CD & Update code structure for runtime stability😃

--- database/database_setting.py
+++ database/database_setting.py
@@ -12,11 +12,11 @@ from sqlalchemy.exc import SQLAlchemyError
 
 
 def get_database_url() -> str:
-    mysql_user = os.getenv("MYSQL_USER")
-    mysql_password = os.getenv("MYSQL_PASSWORD")
-    mysql_host = os.getenv("MYSQL_HOST")
-    mysql_port = os.getenv("MYSQL_PORT")
-    mysql_database_name = os.getenv("MYSQL_DATABASE_NAME")
+    mysql_user = os.getenv("MYSQL_USER")[1:]
+    mysql_password = os.getenv("MYSQL_PASSWORD")[1:]
+    mysql_host = os.getenv("MYSQL_HOST")[1:]
+    mysql_port = os.getenv("MYSQL_PORT")[1:]
+    mysql_database_name = os.getenv("MYSQL_DATABASE_NAME")[1:]
     
     return f"mysql+aiomysql://${mysql_user}:${mysql_password}@${mysql_host}:${mysql_port}/${mysql_database_name}"
 
